5a918e0ceca217e13f33b0f84047fbc10fd0b7ab
cwx2810 <cwx2810@163.com>
2017-09-22 09:37:58 -0700

    获取页码

--- spider.py
+++ spider.py
@@ -1,4 +1,6 @@
+import re
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,20 +10,26 @@ wait = WebDriverWait(browser, 10)
 
 #模拟搜索
 def search():
-    browser.get('http://www.taobao.com')
-    #设置最长加载时间
-    input = wait.until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "#q"))
-    )
-    submit = wait.until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_TSearchForm > div.search-button > button'))
-    )
-    #模拟输入、点击
-    input.send_keys('美食')
-    submit.click()
+    try:
+        browser.get('http://www.taobao.com')
+        #等待搜索框加载
+        input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#q')))
+        #等待查询按钮加载
+        submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_TSearchForm > div.search-button > button')))
+        #模拟输入、点击
+        input.send_keys('美食')
+        submit.click()
+        #等待总页码加载
+        total = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > div.total')))
+        return total.text
+    except TimeoutException:
+        return search()
 
 def main():
-    search()
+    total = search()
+    #在共100页中匹配出100数字
+    total = int(re.compile('(\d+)').search(total).group(1))
+    print(total)
 
 if __name__ == '__main__':
     main()
